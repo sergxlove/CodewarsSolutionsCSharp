c3ef555155f1904780259a615dce3d51aea8be8d
sergxlove <sergxlove@mail.ru>
2024-11-30 21:32:08 +0600

    Добавьте файлы проекта.

---
+++ CodewarsSolutionsCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodewarsSolutionsCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
